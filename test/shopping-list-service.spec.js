961670d03b5170fc3e511fe642b17bb53b31f469
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2019-12-11 09:47:27 -0800

    test for getAllItems method on the ShoppingListService object

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -5,8 +5,41 @@ const knex = require("knex");
 
 const ShoppingListService = require("../src/shopping-list-service");
 
-describe(`Articles service object`, function() {
-  it(`should run the tests`, () => {
-    expect(true).to.eql(false);
+describe(`Shopping List Items service object`, function() {
+  let db;
+  let testItems = [
+    {
+      name: "soap",
+      price: 10.0,
+      date_added: new Date("2019-12-11 12:00:00"),
+      category: "snack"
+    },
+    {
+      name: "cereal",
+      price: 1.0,
+      date_added: new Date("2019-12-11 12:00:00"),
+      category: "breakfast"
+    }
+  ];
+
+  before(() => {
+    db = knex({
+      client: "pg",
+      connection: process.env.TEST_DB_URL
+    });
+  });
+
+  before(() => db("shopping_list").truncate());
+
+  after(() => db.destroy());
+
+  afterEach(() => db("shopping_list").truncate());
+
+  context("Given shopping list with no data, returns empty array", () => {
+    it("getAllItems() resolves an empty array", () => {
+      return ShoppingListService.getAllItems(db).then(actual => {
+        expect(actual).to.eql([]);
+      });
+    });
   });
 });
