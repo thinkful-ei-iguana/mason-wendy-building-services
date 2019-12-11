4618571eb09cdcd4aa8a11a727953d23d68a43da
Mason Reichbauer <mreichba@mail.usf.edu>
2019-12-11 15:10:47 -0500

    finished with insert()

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,7 +1,20 @@
 const ShoppingListService = {
   getAllItems(knex) {
     return knex.select("*").from("shopping_list");
-  }
+  },
+
+  insertItem(knex, newItem) {
+    return knex
+      .insert(newItem)
+      .into("shopping_list")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+
+
+
 };
 
 module.exports = ShoppingListService;
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -5,20 +5,24 @@ const knex = require("knex");
 
 const ShoppingListService = require("../src/shopping-list-service");
 
-describe(`Shopping List Items service object`, function() {
+describe("Shopping List Items service object", function () {
   let db;
   let testItems = [
     {
+      id: 1,
       name: "soap",
-      price: 10.0,
+      price: "10.00",
       date_added: new Date("2019-12-11 12:00:00"),
-      category: "snack"
+      category: "Snack",
+      checked: false
     },
     {
+      id: 2,
       name: "cereal",
-      price: 1.0,
+      price: "1.00",
       date_added: new Date("2019-12-11 12:00:00"),
-      category: "breakfast"
+      category: "Breakfast",
+      checked: false
     }
   ];
 
@@ -42,4 +46,42 @@ describe(`Shopping List Items service object`, function() {
       });
     });
   });
+
+  context("Given shopping list has data", () => {
+    before(() => {
+      return db.insert(testItems)
+        .into("shopping_list");
+    });
+
+    it("GetAllItem() resolves all items from shopping_list table", () => {
+      return ShoppingListService.getAllItems(db).then(actual => {
+        expect(actual).to.eql(testItems);
+
+      });
+    });
+
+    it("insertItem() inserts new item into shopping_list", () => {
+      const newItem = {
+
+        name: "soap",
+        price: "10.00",
+        date_added: new Date("2019-12-11 12:00:00"),
+        category: "Snack",
+
+      };
+      return ShoppingListService.insertItem(db, newItem)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: 1,
+            checked: false,
+            name: newItem.name,
+            price: newItem.price,
+            date_added: new Date(newItem.date_added),
+            category: newItem.category
+          });
+        });
+    });
+
+  });
+
 });
